7674fd49198b23eae034c8ea0ace7501a1cf639a
pablopenrose <local@kader.email>
2021-06-14 15:58:39 -0400

    blipped the board in the One component.

--- src/comps/One.js
+++ src/comps/One.js
@@ -3,13 +3,17 @@ import {useHistory} from 'react-router-dom'
 import Entry from './Entry'
 import {axios} from "axios"
 
+import Chessboard from "chessboardjsx"
+
 function One (history) {
     let hist = useHistory(history)
     let [moves, setMoves] = useState("") 
     useEffect (() => {
         setMoves(read_board_states())
     },[])
-    //const boards = moves.split('\n')
+
+    let datalist = []
+    const [count, increment] = useState(0)
 
     async function read_board_states () {
 
@@ -17,15 +21,27 @@ function One (history) {
         let res = await axios.get("http://localhost:3000/fen/fen.txt")
         let data = res.data
         data = data.split('\n')
-        data = data.splice(0,3)
-        console.log(data)
-        }
+        datalist = data.splice(0,3)
+        console.log(datalist)
+    }
 
     return (
         <div className="wrapper">
+            {render_board(datalist[0])}
             <h2 id="back" onClick={(() => {hist.goBack('')})} >back</h2> 
         </div>
     );
+}
+
+function render_board (pos) {
+    return (
+        <div id="chess">
+            <Chessboard
+                width={500}
+                position={pos}
+            />
+        </div>
+    )
 }
 
 export default One;
